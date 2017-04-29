cc65a85fb681c9fdfeaa3fc2b6634c9efbecf298
BasakFulya <basakfulyaytu@gmail.com>
2017-04-29 08:21:39 -0700

    Add files via upload

---
+++ assignment.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+
+namespace assignment
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            int a = 15, b;
+            b = a++;
+
+            Console.WriteLine(a);
+            Console.WriteLine(b);
+            Console.ReadKey();
+
+        }
+    }
+}
