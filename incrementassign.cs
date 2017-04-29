027c9ab1228fb141334a442a2c940f42b478db70
BasakFulya <basakfulyaytu@gmail.com>
2017-04-29 07:26:34 -0700

    Add files via upload

---
+++ incrementassign.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace incrementassign
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //assign operand
+
+            int a = 5, b, c;
+            b = a;
+            c = a + b;
+            Console.WriteLine(c);
+            Console.ReadKey();
+
+        }
+    }
+}
