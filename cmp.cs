010e94825279c8eb09888d299881c63147174e12
BasakFulya <basakfulyaytu@gmail.com>
2017-04-29 09:54:48 -0700

    Add files via upload

---
+++ cmp.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace CMP
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            //comparing n logical operands
+            //and => && , or => ||,  not => !
+
+
+            bool num1 = (65 < 64);
+
+            Console.WriteLine(num1);
+            Console.ReadKey();
+
+        }
+    }
+}
