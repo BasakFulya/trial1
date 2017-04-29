ecf35790886d310ae0442f1c44f565de0719aebc
BasakFulya <basakfulyaytu@gmail.com>
2017-04-29 06:55:23 -0700

    Add files via upload

--- Program.cs
+++ Program.cs
@@ -11,7 +11,7 @@ namespace mode_process
         {
             //arithmetic operands
             int a = 5, b = 4, c = 19, d = 8;
-            Console.WriteLine("mode of a according to b: {0} n mode of c according to d: {1}",a%b, c%d);
+            Console.WriteLine("mode of a according to b: {0}\nmode of c according to d: {1}",a%b, c%d);
             Console.ReadLine();
         }
     }
