4962ef4a4fcd6e52527b453bffbb139a6ba07d29
BasakFulya <basakfulyaytu@gmail.com>
2017-04-29 07:11:49 -0700

    Add files via upload

---
+++ mode process.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace mode_process
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //arithmetic operands
+            int a = 5, b = 4, c = 19, d = 8;
+            Console.WriteLine("mode of a according to b: {0}\nmode of c according to d: {1}",a%b, c%d);
+            Console.ReadLine();
+        }
+    }
+}
