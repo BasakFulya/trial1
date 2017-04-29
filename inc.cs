4cff175bd26e4acb445570e4a277d79567a40bd1
BasakFulya <basakfulyaytu@gmail.com>
2017-04-29 08:20:35 -0700

    Add files via upload

---
+++ inc.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+namespace inc
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //assign operand
+            int a = 3;
+            a = a + 5;
+            a += 5;
+            Console.WriteLine(a);
+            Console.ReadKey();
+
+        }
+    }
+}
