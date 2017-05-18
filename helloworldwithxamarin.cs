a18a77c58aa7f1a8d16395405e764097b4e6cb0a
Başak Fulya <basakfulyaytu@gmail.com>
2017-05-17 17:22:33 -0700

    Add files via upload

---
+++ helloworldwithxamarin.cs
@@ -0,0 +1,19 @@
+﻿using Android.App;
+using Android.Widget;
+using Android.OS;
+
+namespace Hello_World_with_Xamarin
+{
+    [Activity(Label = "Hello_World_with_Xamarin", MainLauncher = true, Icon = "@drawable/icon")]
+    public class MainActivity : Activity
+    {
+        protected override void OnCreate(Bundle bundle)
+        {
+            base.OnCreate(bundle);
+
+            // Set our view from the "main" layout resource
+            // SetContentView (Resource.Layout.Main);
+        }
+    }
+}
+
