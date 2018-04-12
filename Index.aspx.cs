6fad9197a86dbfae0f18149e854f91ba0a892402
Başak Fulya <basakfulyaytu@gmail.com>
2018-04-13 02:30:45 +0300

    Add files via upload

---
+++ Index.aspx.cs
@@ -0,0 +1,88 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.Data.SqlClient;
+using System.Data;
+
+namespace User_Registration
+{
+    public partial class Index : System.Web.UI.Page
+    {
+        string connectionString = @"Data Source=USER; Initial Catalog=UserRegistrationDB; Integrated Security=True;";
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            if (!IsPostBack)
+            {
+                Clear();
+                if (!String.IsNullOrEmpty(Request.QueryString["id"]))
+                {
+                    int UserID = Convert.ToInt32(Request.QueryString["id"]);
+                    using (SqlConnection sqlCon = new SqlConnection(connectionString))
+                    {
+                        sqlCon.Open();
+                        SqlDataAdapter sqlDa = new SqlDataAdapter("UserViewByID",sqlCon);
+                        sqlDa.SelectCommand.CommandType = CommandType.StoredProcedure;
+                        sqlDa.SelectCommand.Parameters.AddWithValue("@UserID",UserID);
+                        DataTable dtbl = new DataTable();
+                        sqlDa.Fill(dtbl);
+
+
+                        hfUserID.Value = UserID.ToString();
+                        txtFirstName.Text = dtbl.Rows[0][1].ToString();
+                        txtLastName.Text = dtbl.Rows[0][2].ToString();
+                        txtContact.Text = dtbl.Rows[0][3].ToString();
+                        ddlGender.Items.FindByValue(dtbl.Rows[0][4].ToString());
+                        txtAddress.Text = dtbl.Rows[0][5].ToString();
+                        txtUserName.Text = dtbl.Rows[0][6].ToString();
+                        txtPassword.Text = dtbl.Rows[0][7].ToString();
+                        txtPassword.Attributes.Add("value",dtbl.Rows[0][7].ToString());
+                        txtConfirmPassword.Text = dtbl.Rows[0][7].ToString();
+                        txtConfirmPassword.Attributes.Add("value", dtbl.Rows[0][7].ToString());
+
+                    }
+                }
+            }
+        }
+
+        protected void btnSubmit_Click(object sender, EventArgs e)
+        {
+            if (txtUserName.Text == "" || txtPassword.Text == "")
+                lblErrorMessage.Text = "Please Fill Mandatory Fields";
+            else if (txtPassword.Text != txtConfirmPassword.Text)
+                lblErrorMessage.Text = "Password do not match";
+            else
+            {
+                using (SqlConnection sqlCon = new SqlConnection(connectionString))
+                {
+                    sqlCon.Open();
+                    SqlCommand sqlcmd = new SqlCommand("UserAddOrEdit", sqlCon);
+                    sqlcmd.CommandType = CommandType.StoredProcedure;
+                    sqlcmd.Parameters.AddWithValue("@UserID", Convert.ToInt32(hfUserID.Value == "" ? "0" : hfUserID.Value));
+                    sqlcmd.Parameters.AddWithValue("@FirstName", txtFirstName.Text.Trim());
+                    sqlcmd.Parameters.AddWithValue("@LastName", txtLastName.Text.Trim());
+                    sqlcmd.Parameters.AddWithValue("@Contact", txtContact.Text.Trim());
+                    sqlcmd.Parameters.AddWithValue("@Gender", ddlGender.SelectedValue);
+                    sqlcmd.Parameters.AddWithValue("@Address", txtAddress.Text.Trim());
+                    sqlcmd.Parameters.AddWithValue("@UserName", txtUserName.Text.Trim());
+                    sqlcmd.Parameters.AddWithValue("@Password", txtPassword.Text.Trim());
+                    sqlcmd.ExecuteNonQuery();
+                    Clear();
+                    lblSuccessMessage.Text = "Submittted Succcessfully";
+                }
+            }
+            
+
+            
+            
+        }
+        void Clear()
+        {
+            txtFirstName.Text = txtLastName.Text = txtContact.Text = txtAddress.Text = txtUserName.Text = txtPassword.Text = txtConfirmPassword.Text = "";
+            hfUserID.Value = "";
+            lblSuccessMessage.Text = lblErrorMessage.Text = "";
+        }
+    }
+}
